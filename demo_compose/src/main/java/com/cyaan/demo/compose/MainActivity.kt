c4dbd112f46203323069dd3156d8159a38384a4a
Cyaan <cyaanchen@gmail.com>
2022-07-29 18:36:08 +0800

    compose codelabs 1

--- demo_compose/src/main/java/com/cyaan/demo/compose/MainActivity.kt
+++ demo_compose/src/main/java/com/cyaan/demo/compose/MainActivity.kt
@@ -3,14 +3,18 @@ package com.cyaan.demo.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.cyaan.demo.compose.ui.theme.DemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,26 +23,95 @@ class MainActivity : ComponentActivity() {
         setContent {
             DemoTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                myApp()
             }
-
-
         }
-
-
     }
+}
+
+@Composable
+fun myApp() {
+    var showWelcome by rememberSaveable { mutableStateOf(true) }
+    if (showWelcome) {
+        WelcomeContent {
+            showWelcome = false
+        }
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun WelcomeContent(onWelClick: () -> Unit = {}) {
+    Column(
+        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to compose!")
+        Button(modifier = Modifier.padding(top = 24.dp), onClick = onWelClick) {
+            Text(text = "choose")
+        }
+    }
+}
 
 
+@Composable
+fun Greetings(
+    text: List<String> = List(20) {
+        "No.$it"
+    }
+) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = text) { num ->
+            Greeting(name = num)
+        }
+    }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Column() {
-        for (i in 0..5) {
-            Text(text = "Hello $name!")
+    var expanded by remember { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(if (expanded) 48.dp else 0.dp)
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+                .padding(bottom = extraPadding)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .align(CenterVertically)
+
+            ) {
+                Text(text = "Hello,")
+                Text(text = "$name")
+            }
+            OutlinedButton(
+                onClick = {
+                    expanded = !expanded
+                }, modifier = Modifier
+                    .height(60.dp)
+                    .width(120.dp)
+            ) {
+                Text(text = if (expanded) "show less" else "show more")
+            }
         }
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun MyAppPreview() {
+    DemoTheme {
+        myApp()
     }
 }
 
@@ -46,6 +119,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     DemoTheme {
-        Greeting("Android")
+        Greeting("android")
     }
 }
