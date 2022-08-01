44ab03f398dd49bcff794ea4ddd419a3c23f0275
Cyaan <cyaanchen@gmail.com>
2022-08-01 14:31:55 +0800

    compose codelabs

--- demo_compose/src/main/java/com/cyaan/demo/compose/MainActivity.kt
+++ demo_compose/src/main/java/com/cyaan/demo/compose/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.cyaan.demo.compose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cyaan.demo.compose.ui.theme.DemoTheme
@@ -38,6 +49,21 @@ fun myApp() {
         }
     } else {
         Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "dark")
+@Composable()
+fun myAppDark() {
+    DemoTheme {
+        var showWelcome by rememberSaveable { mutableStateOf(true) }
+        if (showWelcome) {
+            WelcomeContent {
+                showWelcome = false
+            }
+        } else {
+            Greetings()
+        }
     }
 }
 
@@ -70,9 +96,12 @@ fun Greetings(
 
 @Composable
 fun Greeting(name: String) {
-    var expanded by remember { mutableStateOf(false) }
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
-    val extraPadding by animateDpAsState(if (expanded) 48.dp else 0.dp)
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow)
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -82,7 +111,7 @@ fun Greeting(name: String) {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(24.dp)
-                .padding(bottom = extraPadding)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
         ) {
             Column(
                 modifier = Modifier
@@ -92,15 +121,24 @@ fun Greeting(name: String) {
             ) {
                 Text(text = "Hello,")
                 Text(text = "$name")
+                if (expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4)
+                    )
+                }
             }
-            OutlinedButton(
+            IconButton(
                 onClick = {
                     expanded = !expanded
-                }, modifier = Modifier
-                    .height(60.dp)
-                    .width(120.dp)
+                }
             ) {
-                Text(text = if (expanded) "show less" else "show more")
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowDown else Icons.Filled.KeyboardArrowUp,
+                    contentDescription = if (expanded) stringResource(id = R.string.show_less) else stringResource(id = R.string.show_more)
+                )
+
+//                Text(text = if (expanded) stringResource(id = R.string.show_less) else stringResource(id = R.string.show_more))
             }
         }
     }
