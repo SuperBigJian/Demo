796aa2756d718941ea6001e2422dd90c755f6e1e
Cyaan <cyaanchen@gmail.com>
2022-08-24 18:40:03 +0800

    add:breakpad demo

---
+++ libraries/breakpad/src/main/java/com/cyaan/lib/breakpad/BreakpadDumper.java
@@ -0,0 +1,13 @@
+package com.cyaan.lib.breakpad;
+
+public class BreakpadDumper {
+    static {
+        System.loadLibrary("breakpad-core");
+    }
+
+    public static void initBreakpad(String path) {
+        initBreakpadNative(path);
+    }
+
+    private static native void initBreakpadNative(String path);
+}
